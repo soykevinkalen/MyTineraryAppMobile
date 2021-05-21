10b9283565ed529b641a5cde89b895f21d344e25
Kevin Baez <soykevinkalen@gmail.com>
2021-05-20 21:08:47 -0300

    home lista

--- Pages/Home.js
+++ Pages/Home.js
@@ -1,11 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Button, Image, ScrollView } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { StyleSheet, Text, View, Button, Image, ScrollView, Dimensions, TouchableOpacity } from 'react-native';
 import Header from '../Components/Header'
 import {Icon} from 'react-native-elements'
+import Carousel, {ParallaxImage} from 'react-native-snap-carousel'
+
+const cities = [
+    {title: 'London', illustration: 'https://i.ibb.co/zGv36sy/london.jpg'},
+    {title: 'New York', illustration: 'https://i.ibb.co/0cbSkXW/newyork.jpg'},
+    {title: 'Paris', illustration: 'https://i.ibb.co/JHhr5wq/paris.jpg'},
+    {title: 'Dubai', illustration: 'https://i.ibb.co/Xx1tTgj/dubai.jpg'},
+    {title: 'Tokyo', illustration: 'https://i.ibb.co/WsSZhyC/tokyo.jpg'},
+    {title: 'Moscow', illustration: 'https://i.ibb.co/Gk0D0cT/moscow.jpg'},
+    {title: 'Tauranga', illustration: 'https://i.ibb.co/ZxBrYXb/tauranga.jpg'},
+    {title: 'Queenstown', illustration: 'https://i.ibb.co/1z78FLJ/queenstown.jpg'},
+    {title: 'Los Angeles', illustration: 'https://i.ibb.co/09jkWVh/los-Angeles.jpg'},
+    {title: 'Madrid', illustration: 'https://i.ibb.co/JB21cDx/madrid.jpg'},
+    {title: 'Rome', illustration: 'https://i.ibb.co/GcZSmCb/rome.jpg'},
+    {title: 'Barcelona', illustration: 'https://i.ibb.co/mTSvDHG/barcelona.jpg'}          
+]
+
+const {width: screenWidth} = Dimensions.get('window');
 
 const Home = (props) => {
     
+    const [city, setCity] = useState([])
+
+    useEffect(() => {
+        setCity(cities)
+    },[])
     
+    const carouselRef = useRef(null);
+
+    const goForward = () => {
+        carouselRef.current.snapToNext();
+    };
+
+    const renderItem = ({item, index}, parallaxProps) => {
+        return (
+          <View style={styles.itemCarrousel}>
+            <ParallaxImage
+              source={{uri: item.illustration}}
+              containerStyle={styles.imageContainerCarrousel}
+              style={styles.imageCarrousel}
+              parallaxFactor={0.4}
+              {...parallaxProps}
+            />
+            <Text style={styles.title} numberOfLines={2}>
+                {item.title}
+            </Text>
+          </View>
+        );
+    };
+
     return(        
         <View style={styles.container}>
             <Header />
@@ -34,12 +80,55 @@ const Home = (props) => {
                         <Text style={styles.buttonCallToAction} title="Cities" onPress={() => props.navigation.navigate('cities')}>Cities</Text>
                     </View>
                 </View>
+
+                <View style={styles.containerCarrousel}>
+                    <TouchableOpacity onPress={goForward}>
+                    
+                    </TouchableOpacity>
+                    <Carousel
+                        ref={carouselRef}
+                        sliderWidth={screenWidth}
+                        sliderHeight={screenWidth}
+                        itemWidth={screenWidth - 100}
+                        data={city}
+                        loop={true}
+                        autoplay={true}
+                        renderItem={renderItem}
+                        hasParallaxImages={true}
+                    />
+                  
+                </View>
             </ScrollView>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    title:{
+        position: "absolute",
+        width: "100%",
+        textAlign: "center",
+        top: "50%",
+        backgroundColor: "#aeafafab",
+        fontSize: 20
+    },
+    containerCarrousel: {
+        flex: 1,
+    },
+    itemCarrousel: {
+        width: screenWidth - 82,
+        height: screenWidth - 82,
+    },
+    imageContainerCarrousel: {
+        flex: 1,
+        marginBottom: Platform.select({ios: 0, android: 1}), // Prevent a random Android rendering issue
+        backgroundColor: 'white',
+        borderRadius: 8,
+    },
+    imageCarrousel: {
+        ...StyleSheet.absoluteFillObject,
+        resizeMode: 'cover',
+    },
     buttonCallToAction:{
         fontSize: 15,
         color:"#141823",
@@ -81,13 +170,9 @@ const styles = StyleSheet.create({
         width: '100%'
     },
     container: {
-        // width: '100%',
         flex:1,
-        // backgroundColor: '#000',
-        // flexDirection: 'row',
-        alignItems:'center',
-        // justifyContent: 'space-around',
-        height: '100%'    
+        height: '100%',
+        marginBottom: 10    
     },
     text: {
         color: 'white',
@@ -99,16 +184,11 @@ const styles = StyleSheet.create({
     picture: {
         width: '100%',
         height: 500,
-        // marginBottom: 20
     },
     containerIconText: {
         position:"absolute",
-        // margin:"auto"
-        // width: '100%',
-        // height: '50%',
         width:"100%",
         backgroundColor: "#aeafafab",
-        // flexDirection: 'row',
         justifyContent: "center",
         alignItems: "center",
     },
